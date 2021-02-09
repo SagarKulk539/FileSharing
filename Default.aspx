3d7d905778b56e619bcb8ce2a53d0614c5d38abc
Sagar Kulkarni <44294928+SagarKulk539@users.noreply.github.com>
2021-02-09 10:59:38 +0530

    First Commit

---
+++ Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="File Sharing" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FileSharing._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div style="margin-top:0px" class="jumbotron">
+        <h1>File Sharing</h1>
+        <p class="lead">Upload and Download files with peers</p>
+    </div>
+    <div>
+        <asp:FileUpload ID="FileUpload1" runat="server" Height="25px" Width="206px" />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" />
+    </div>
+    
+    <br />
+    <asp:GridView style="font-size:18px" ID="GridView1" CssClass="table" runat="server" AutoGenerateColumns="False" OnRowCommand="GridView1_RowCommand" BackColor="White" BorderColor="Black" BorderStyle="Solid" BorderWidth="1px" CellPadding="4" CellSpacing="2" ForeColor="Black" GridLines="Horizontal" Height="162px" HorizontalAlign="Left" Width="512px">
+        <Columns>
+            <asp:TemplateField HeaderText="File">
+                <ItemTemplate>
+                    <asp:LinkButton ID="LinkButton1" runat="server" CommandArgument='<%# Eval("File") %>' CommandName="Download" Text='<%# Eval("File") %>'></asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:BoundField DataField="Size" HeaderText="Size in Bytes" />
+        </Columns>
+        <FooterStyle BackColor="#CCCC99" ForeColor="Black" />
+        <HeaderStyle BackColor="#333333" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="White" ForeColor="Black" HorizontalAlign="Right" />
+        <SelectedRowStyle BackColor="#CC3333" Font-Bold="True" ForeColor="White" />
+        <SortedAscendingCellStyle BackColor="#F7F7F7" />
+        <SortedAscendingHeaderStyle BackColor="#4B4B4B" />
+        <SortedDescendingCellStyle BackColor="#E5E5E5" />
+        <SortedDescendingHeaderStyle BackColor="#242121" />
+    </asp:GridView>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+
+namespace FileSharing
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(Server.MapPath("~/Data/"));
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("File", typeof(string));
+            dt.Columns.Add("Size", typeof(string));
+
+            foreach (string strFile in Directory.GetFiles(Server.MapPath("~/Data/")))
+            {
+                FileInfo fi = new FileInfo(strFile);
+
+                dt.Rows.Add(fi.Name, fi.Length);
+            }
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(Server.MapPath("~/Data/"));
+
+            if (FileUpload1.HasFile)
+            {
+                try
+                {
+                    if (FileUpload1.PostedFile.ContentLength <= 5242880)
+                    {
+                        byte flag = 0;
+                        foreach (string strFile in Directory.GetFiles(Server.MapPath("~/Data/")))
+                        {
+                            FileInfo fi = new FileInfo(strFile);
+                            if(fi.Name.Equals(FileUpload1.FileName))
+                            {
+                                ScriptManager.RegisterStartupScript(this, this.GetType(), "redirect", "alert('File Already Exists!'); window.location='" + Request.ApplicationPath + "Default.aspx';", true);
+                                flag = 1;
+                                break;
+                            }
+                        }
+                        if(flag==0)
+                        {
+                            FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Data/") + FileUpload1.FileName);
+                            //FileUpload1.PostedFile.SaveAs(HttpContext.Current.Server.MapPath(Request.ApplicationPath + "/Content/Data/") + FileUpload1.FileName);
+                            Response.Redirect(Request.ApplicationPath + "Default.aspx");
+                        }
+                            
+                            
+                    }
+                    else
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "redirect", "alert('File Size Limit : 5 MB'); window.location='" + Request.ApplicationPath + "Default.aspx';", true);
+                }
+                catch(HttpException)
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "redirect", "alert('File Size Limit : 5 MB'); window.location='" + Request.ApplicationPath + "Default.aspx';", true);
+                }
+                
+            }
+            else 
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "redirect", "alert('Select File to Upload'); window.location='" + Request.ApplicationPath + "Default.aspx';", true);
+            }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("File", typeof(string));
+            dt.Columns.Add("Size", typeof(string));
+
+            foreach (string strFile in Directory.GetFiles(Server.MapPath("~/Data/")))
+            {
+                FileInfo fi = new FileInfo(strFile);
+
+                dt.Rows.Add(fi.Name, fi.Length);
+            }
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName=="Download")
+            {
+                Response.Clear();
+                Response.ContentType = "application/octect-stream";
+                Response.AppendHeader("content-disposition", "filename=" + e.CommandArgument);
+                Response.End();
+            }
+        }
+    }
+}
